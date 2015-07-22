0c6a717d0cb110ef575a2e7369a8ff9610d4eb4a
abhirup2612 <abhirupkhanna@yahoo.com>
2015-07-22 16:43:38 +0530

    Update DecisionGetSet.java

--- Middle/CAT-REST_Service/src/main/java/cat/xebia/domain/DecisionGetSet.java
+++ Middle/CAT-REST_Service/src/main/java/cat/xebia/domain/DecisionGetSet.java
@@ -1,40 +1,59 @@
-package cat.xebia.domain;
+package abhirup.khanna;
 
-import java.io.Serializable;
+public class DecisionGetSet {
 
-public class DecisionGetSet implements Serializable {
+	static int num_level,questions_level,num_students,Min_Ques_To_Attempt,Current_Level,min_pass_marks,total_num_categories,i,T;
 
-	private static final long serialVersionUID = -4741633254571866662L;
-	String question;
-	String answer;
-	String level;
-	String category;
-	
-	
-	public String getQuestion() {
-		return question;
-	}
-	public void setQuestion(String question) {
-		this.question = question;
-	}
-	public String getAnswer() {
-		return answer;
-	}
-	public void setAnswer(String answer) {
-		this.answer = answer;
-	}
-	public String getLevel() {
-		return level;
-	}
-	public void setLevel(String level) {
-		this.level = level;
-	}
-	public String getCategory() {
-		return category;
-	}
-	public void setCategory(String category) {
-		this.category = category;
+	public static int getNum_level() {
+		return num_level;
 	}
 
+	public static void setNum_level(int num_level) {
+		decision.num_level = num_level;
+	}
+
+	public static int getQuestions_level() {
+		return questions_level;
+	}
+
+	public static void setQuestions_level(int questions_level) {
+		decision.questions_level = questions_level;
+	}
+
+	public static int getNum_students() {
+		return num_students;
+	}
+
+	public static void setNum_students(int num_students) {
+		decision.num_students = num_students;
+	}
+
+	public static int getCurrent_Level() {
+		return Current_Level;
+	}
+
+	public static void setCurrent_Level(int current_Level) {
+		Current_Level = current_Level;
+	}
+
+	public static int getMin_pass_marks() {
+		return min_pass_marks;
+	}
+
+	public static void setMin_pass_marks(int min_pass_marks) {
+		decision.min_pass_marks = min_pass_marks;
+	}
+
+	public static int getTotal_num_categories() {
+		return total_num_categories;
+	}
+
+	public static void setTotal_num_categories(int total_num_categories) {
+		decision.total_num_categories = total_num_categories;
+	}
+
+	public static int getT() {
+		return T;
+	}
 	
 }
